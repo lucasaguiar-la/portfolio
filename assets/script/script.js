3df6a88875a015b34f6722223ba71ac77af42942
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2023-06-21 23:00:24 -0300

    ajustes header

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,30 @@
-window.addEventListener("scroll", function () {
-  let headerNav = document.querySelector(".header");
-
-  if (window.scrollY > 0) {
-    headerNav.classList.add("fixed-header");
-  } else {
-    headerNav.classList.remove("fixed-header");
-  }
-});
-
+// Menu lateral
 const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".navMenu");
 
 menuIcon.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+window.addEventListener("scroll", function () {
+  let headerNav = document.querySelector("header");
+
+  if (window.scrollY > 0) {
+    headerNav.classList.add(
+      "fixed-header",
+      "fixed-header-transition",
+      "fixed-header-background"
+    );
+  } else {
+    headerNav.classList.remove("fixed-header-background");
+  }
+});
+
+// Tema claro e escuro
+function switchTheme() {
+  document.body.classList.toggle("isLight");
+  document.body.classList.toggle("isDark");
+}
+document
+  .getElementById("switchThemeBtn")
+  .addEventListener("click", switchTheme);
