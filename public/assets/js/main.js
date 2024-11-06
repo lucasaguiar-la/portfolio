7bc79f261e79249aeab6bfc7c365334fc79278a3
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2024-11-06 15:27:57 -0300

    refac: Atualização do módulo fadeIn.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,15 @@
 import { ThemeManager } from './modules/switchs.js';
+import { FadeInManager } from './modules/fadeIn.js';
 
 class App {
     constructor() {
         this.themeManager = new ThemeManager();
+        this.fadeInManager = new FadeInManager();
     }
 
     init() {
         this.themeManager.init();
+        this.fadeInManager.init();
     }
 }
 
--- public/assets/js/modules/fadeIn.js
+++ public/assets/js/modules/fadeIn.js
@@ -1,20 +1,22 @@
+export class FadeInManager {
+    constructor() {
+        this.fadeIns = document.querySelectorAll(".fade-in");
+    }
 
-// [EFEITO DE CARREGAMENTO DA PÁGINA]
-document.addEventListener("DOMContentLoaded", function() {
-    const fadeIns = document.querySelectorAll(".fade-in");
-
-    const observador = new IntersectionObserver((entrada, observador) => {
-        entrada.forEach(entrada => {
-            if (entrada.isIntersecting) {
-                entrada.target.classList.add("start");
-                observador.unobserve(entrada.target);
-            }
+    init() {
+        const observador = new IntersectionObserver((entrada, observador) => {
+            entrada.forEach(entrada => {
+                if (entrada.isIntersecting) {
+                    entrada.target.classList.add("start");
+                    observador.unobserve(entrada.target);
+                }
+            });
+        }, {
+            threshold: 0.5
         });
-    }, {
-        threshold: 0.5
-    });
 
-    fadeIns.forEach(fadeIn => {
-        observador.observe(fadeIn);
-    });
-});
+        this.fadeIns.forEach(fadeIn => {
+            observador.observe(fadeIn);
+        });
+    }
+}
