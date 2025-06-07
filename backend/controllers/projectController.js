68ce06e9468b2cddd25483f674ca1a977d746a2d
lucasaguiar-la <lucas.aguiar.ads@gmail.com>
2025-06-07 13:04:27 -0300

    fix: Correção UTF-8

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -24,6 +24,7 @@ exports.getProjetos = async (req, res) => {
             };
         }));
 
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
         res.json(projetosComTecnologias);
     } catch (err){
     console.log(err);
